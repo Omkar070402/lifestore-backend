b9f765721aab2977f6845095b199ecd67886643f
omkar070402 <omkarseshadri007@gmail.com>
2025-03-31 14:04:08 +0530

    commit

--- configs/db.js
+++ configs/db.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
  
+      await mongoose.connect(process.env.MONGO_URI)
      mongoose.connection.on("connected",()=>{
       console.log('DB Connected')
      })
 
 
-      await mongoose.connect(process.env.MONGO_URI)
       
 
    
